328b223658c15ebe5301549905413ccd13c37d3d
davisreyno <davis.reyno@gmail.com>
2026-07-16 14:17:42 -0500

    Implement try/catch for Add-Computer command
    Added error handling for domain join process.

--- PowerShell/join-AD.ps1
+++ PowerShell/join-AD.ps1
@@ -2,7 +2,15 @@
 
 $DomainName = ""
 $OUPath = "OU=Workstations,OU=Computers,DC=nicholsequipment,DC=local"
+
 $ComputerName = Read-Host ""
+
+# Verify computer name isn't blank 
+if ([string]::IsNullOrWhiteSpace($ComputerName)) {
+  Write-Host "Computer name cannot be blank."
+  exit
+}
+
 $Credential = Get-Credential -Message "Enter an account authorized to join computers to the domain" 
 
 # Rename 
@@ -10,12 +18,18 @@ Rename-Computer `
   -NewName $ComputerName `
   -Force
 
-# Join AD
-Add-Computer `
-  -DomainName $DomainName `
-  -Credential $Credential `
-  -OUPath $OUPath `
-  -NewName $ComputerName `
-  -Restart `
-  -Force
- 
+# Join AD try/catch 
+try {
+  Add-Computer `
+    -DomainName $DomainName `
+    -Credential $Credential `
+    -OUPath $OUPath `
+    -NewName $ComputerName `
+    -Restart `
+    -Force
+    -ErrorAction Stop # Treats any failures as a terminating error so catch block runs
+  }
+  catch {
+    Write-Host "Failed to join the domain."
+    Write-Host $_.Exception.Message
+  }
